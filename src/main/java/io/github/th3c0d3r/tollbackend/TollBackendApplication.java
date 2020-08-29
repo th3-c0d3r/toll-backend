ec6cd5ae366ac4693064eed087ceedfbdfd08faa
karthikeyan <karthikeyan@ninjacart.com>
2020-08-29 23:28:27 +0530

    initial commit

---
+++ src/main/java/io/github/th3c0d3r/tollbackend/TollBackendApplication.java
@@ -0,0 +1,13 @@
+package io.github.th3c0d3r.tollbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TollBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TollBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/th3c0d3r/tollbackend/TollBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.th3c0d3r.tollbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TollBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
