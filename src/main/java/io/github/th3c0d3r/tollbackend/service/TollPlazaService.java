fc33b4c8b4ca52ce95692bcf5f88f2dff3d406d5
karthikeyan <karthikeyan@ninjacart.com>
2020-08-30 19:02:03 +0530

    chenged thread sleep to 1 sec

--- src/main/java/io/github/th3c0d3r/tollbackend/service/TollPlazaService.java
+++ src/main/java/io/github/th3c0d3r/tollbackend/service/TollPlazaService.java
@@ -173,7 +173,7 @@ public class TollPlazaService {
                     && reverseGeoCodeDto.getAddress().getState() != null && isValidISOLatin1(reverseGeoCodeDto.getAddress().getState()))
                     ? reverseGeoCodeDto.getAddress().getState()
                     : "not_found");
-            Thread.sleep(800);
+            Thread.sleep(1000);
 
         }
 
