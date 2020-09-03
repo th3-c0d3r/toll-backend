bb0b0455a58093bbc4a2fecb81d7a3186a003719
karthikeyan <karthikeyan@ninjacart.com>
2020-09-03 17:28:40 +0530

    endpoints change

--- src/main/java/io/github/th3c0d3r/tollbackend/controller/TollPlazaController.java
+++ src/main/java/io/github/th3c0d3r/tollbackend/controller/TollPlazaController.java
@@ -6,49 +6,48 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.lang.Nullable;
 import org.springframework.web.bind.annotation.*;
 
-import javax.websocket.server.PathParam;
 import java.util.List;
 
 @RestController
-@RequestMapping(path = "/tollPlaza")
+@RequestMapping(path = "/")
 public class TollPlazaController {
 
     @Autowired
     private TollPlazaService tollPlazaService;
 
     @PostMapping
-    @RequestMapping(path = "/cron")
-    public void runCron(){
+    @RequestMapping(path = "/tollPlaza/cron")
+    public void runCron() {
         tollPlazaService.populateTable();
     }
 
     @GetMapping
-    @RequestMapping(path = "/{tollPlazaId}")
+    @RequestMapping(path = "/tollPlaza/{tollPlazaId}")
     public TollPlazaDto getByTollPlazaId(@PathVariable(name = "tollPlazaId") Integer tollPlazaId) throws Exception {
         return tollPlazaService.getByTollPlazaId(tollPlazaId);
     }
 
     @GetMapping
-    @RequestMapping(path = "/test")
+    @RequestMapping(path = "/tollPlaza/test")
     public void test() throws InterruptedException {
         tollPlazaService.reverseGeoCode();
     }
 
     @GetMapping
-    @RequestMapping(path = "/getAll")
-    public List<TollPlazaDto> getByStateAndTollName(@Nullable @RequestParam(name = "stateName") String stateName,@Nullable @RequestParam(name = "tollName") String tollName){
+    @RequestMapping(path = "/tollPlaza/getAll")
+    public List<TollPlazaDto> getByStateAndTollName(@Nullable @RequestParam(name = "stateName") String stateName, @Nullable @RequestParam(name = "tollName") String tollName) {
         return tollPlazaService.getByStateAndTollName(stateName, tollName);
     }
 
     @GetMapping
     @RequestMapping(path = "/getAllTollNames")
-    public List<String> getAllTollPlazaNamesByState(@Nullable @RequestParam(name = "stateName") String stateName){
+    public List<String> getAllTollPlazaNamesByState(@Nullable @RequestParam(name = "stateName") String stateName) {
         return tollPlazaService.getAllTollPlazaNamesByState(stateName);
     }
 
     @GetMapping
     @RequestMapping(path = "/getAllStateNames")
-    public List<String> getAllTollPlazaStateNames(){
+    public List<String> getAllTollPlazaStateNames() {
         return tollPlazaService.getAllTollPlazaStateNames();
     }
 
