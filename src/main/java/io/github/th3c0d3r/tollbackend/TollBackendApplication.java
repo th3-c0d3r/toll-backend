03f47e3b6c8d3033f4936baaabb7e7fae84e5c31
Karthikeyan <emailtokarthi98@gmail.com>
2022-04-09 22:12:32 +0530

    added Swagger discription

--- src/main/java/io/github/th3c0d3r/tollbackend/TollBackendApplication.java
+++ src/main/java/io/github/th3c0d3r/tollbackend/TollBackendApplication.java
@@ -1,5 +1,7 @@
 package io.github.th3c0d3r.tollbackend;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -8,6 +10,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 @EnableAsync
+@OpenAPIDefinition(info = @Info(title = "Toll Plaza India API", version = "V0", description = "GitHub: https://github.com/th3-c0d3r/toll-backend"))
 public class TollBackendApplication {
 
 	@Bean
